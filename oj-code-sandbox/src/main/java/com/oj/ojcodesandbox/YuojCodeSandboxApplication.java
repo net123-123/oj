93a80c72330dd62e0e0d4516400d87956bba2069
net123-123 <55226902+net123-123@users.noreply.github.com>
2023-09-05 14:56:51 +0800

    Add files via upload

---
+++ oj-code-sandbox/src/main/java/com/oj/ojcodesandbox/YuojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.oj.ojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ojCodeSandboxApplication.class, args);
+    }
+
+}
